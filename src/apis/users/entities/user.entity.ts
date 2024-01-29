5c198ff0668c361484a51b641fda273c8e8e9ee5
ssarisong <thdtnwls2989@naver.com>
2024-01-29 19:27:46 +0900

    Add User Entity

--- src/apis/users/entities/user.entity.ts
+++ src/apis/users/entities/user.entity.ts
@@ -1,13 +1,53 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+//Entity: ERD 내에서 정의되어 있는 테이블대로 column을 정의한 클래스, 
+//추가적으로 데이터베이스를 사용하기 위해 typeorm과 graphql 관련 설정도 해줘야 함.
 
-@Entity()
-export class User {
-  @PrimaryGeneratedColumn('uuid')
+import { Field, Int, ObjectType } from "@nestjs/graphql";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"; //typeorm: 데이터베이스 관련
+
+@Entity() //@ 들어간거 다 decorator  |  Entity: typeorm
+@ObjectType() //graphql
+export class User { //export: 외부에서도 이 클래스를 사용할 수 있게 해줌
+  @PrimaryGeneratedColumn('uuid') //기본키, 자동으로 만들어주는 기능(원래는 @PrimaryColumn)
+  @Field(() => String) //graphql, graphql은 괄호 안에 type을 지정해줘야함
   id: string;
 
   @Column()
+  @Field(() => String) 
   email: string;
 
   @Column()
+  @Field(() => String) 
+  name: string;
+
+  @Column()
+  @Field(() => String) 
+  gender: string;
+
+  @Column({ type: 'timestamp' }) //Date 타입은 이런 식으로 하면 돼요
+  @Field(() => Date) 
+  birth_at: Date;
+
+  @Column()
+  @Field(() => String) 
+  mbti: string;
+
+  @Column()
+  @Field(() => String) 
+  phone_number: string;
+
+  @Column()
+  @Field(() => String) 
   password: string;
+
+  @Column()
+  @Field(() => Boolean) 
+  is_find_mate: boolean;
+
+  @Column()
+  @Field(() => Int) 
+  point: number;
+
+  @Column({ type: 'timestamp' }) //Date 타입은 이런 식으로 하면 돼요
+  @Field(() => Date) 
+  create_at: Date;
 }
