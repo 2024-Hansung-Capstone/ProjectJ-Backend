1977b36baa7550be505b54255047d1dbfe42acc3
ssarisong <thdtnwls2989@naver.com>
2024-02-11 14:41:58 +0900

    Update RefreshStrategy to Find RefreshToken

--- src/apis/users/strategies/jwt-refresh.strategy.ts
+++ src/apis/users/strategies/jwt-refresh.strategy.ts
@@ -5,10 +5,19 @@ export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'jaeheoga') {
   constructor() {
     super({
       jwtFromRequest: (req) => {
-        const cookie = req.headers.cookie;
-        const refreshToken = cookie.replace('refreshToken=', '');
-        console.log(refreshToken);
-        //header 안에 cookie의 refreshToken= 부분을 지워주면서 온전한 refreshToken 값을 받아옴
+        let refreshToken = null;
+        if (req && req.headers.cookie) {
+          const cookies = req.headers.cookie
+            .split(';')
+            .map((cookie) => cookie.trim());
+          const refreshTokenCookie = cookies.find((cookie) =>
+            cookie.startWith('refreshToken='),
+          );
+          if (refreshTokenCookie) {
+            refreshToken = refreshTokenCookie.replace('refreshToken=', '');
+            //header 안에 cookie의 refreshToken= 부분을 지워주면서 온전한 refreshToken 값을 받아옴
+          }
+        }
         return refreshToken;
       },
       secretOrKey: 'sujin2',
