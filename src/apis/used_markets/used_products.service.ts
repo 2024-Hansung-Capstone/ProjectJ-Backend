a6f49c6225f4fab39bfcb3b8cf244f2ab4e63118
kimjunghwannn <aa2002dcks@naver.com>
2024-02-18 22:42:25 +0900

    userService를 우선적으로 사용하기 위해 user와 관련된 Used_product_service부분 삭제, Resolver 내에서 context.req.user.id를 가져와 사용, User_product의 update부분에서 Used_product를 바로 리턴하기 위해 findOne사용 delete부분에는 삭제가 잘 되었으면 true를 리턴하는 기능추가, likeUserRecordRepository를 추가하

--- src/apis/used_markets/used_products.service.ts
+++ src/apis/used_markets/used_products.service.ts
@@ -9,10 +9,8 @@ import { CreateProductInput } from './dto/create-used_products.input';
 import { Used_product } from './entities/used_product.entity';
 import { UpdateUsedProductInput } from './dto/update-used_products.input';
 import { SearchProductInput } from './dto/search-used_products.input';
-import { User } from '../users/entities/user.entity';
 import { AddViewProductInput } from './dto/addview_used_products.input';
 import { AddLikeProductInput } from './dto/addlike_used_products.inpput';
-import { IContext } from '../users/interfaces/user-service.interface';
 import { UpdateUsedProductStateInput } from './dto/update-used_products.state.input';
 import { Like_user_record } from './entities/like_user_record.entity';
 import { UserService } from '../users/users.service';
@@ -21,15 +19,19 @@ export class UsedProductService {
   constructor(
     @InjectRepository(Used_product)
     private usedProductRepository: Repository<Used_product>,
+    @InjectRepository(Like_user_record)
+    private likeUserRecordRepository: Repository<Like_user_record>,
     private readonly userService: UserService,
   ) {}
 
   async findAll(): Promise<Used_product[]> {
-    return this.usedProductRepository.find();
+    return await this.usedProductRepository.find({});
   }
 
   async findById(id: string): Promise<Used_product> {
-    return this.usedProductRepository.findOne({ where: { id: id } });
+    return await this.usedProductRepository.findOne({
+      where: { id: id },
+    });
   }
   //유저이름으로 검색 즉 상점이름으로 검색하는 방법
   async findByuser_Id(user_id: string): Promise<Used_product[]> {
@@ -60,7 +62,7 @@ export class UsedProductService {
     if (state) {
       searchConditions.state = state;
     }
-    return this.usedProductRepository.find({ where: searchConditions });
+    return await this.usedProductRepository.find({ where: searchConditions });
   }
 
   async create(
@@ -84,51 +86,62 @@ export class UsedProductService {
   async update(
     id: string,
     usedProduct: UpdateUsedProductInput,
-    context: IContext,
+    user_id: string,
   ): Promise<Used_product> {
     const used_product = await this.findById(id);
+    const { title, price, detail, category, state } = usedProduct;
     if (!used_product) {
       throw new NotFoundException(`Id가 ${id}인 것을 찾을 수 없습니다.`);
     }
-    const user = await this.findUserById(context);
-    if (used_product.user.id !== user.id) {
+    const user = await this.userService.findById(user_id);
+    const now_user = await this.userService.findById(used_product.user.id);
+    if (now_user.id !== user.id) {
       throw new ForbiddenException(
         `본인이 작성한 게시글만 수정할 수 있습니다.`,
       );
     }
-    await this.usedProductRepository.update(id, usedProduct);
-    return this.usedProductRepository.findOne({ where: { id: id } });
+    used_product.title = title;
+    used_product.price = price;
+    used_product.detail = detail;
+    used_product.category = category;
+    used_product.state = state;
+    await this.usedProductRepository.save(used_product);
+    return await this.usedProductRepository.findOne({ where: { id: id } });
   }
 
   async updateDealState(
     UpdateUsed_ProductStateInput: UpdateUsedProductStateInput,
-    context: IContext,
+    user_id: string,
   ): Promise<Used_product> {
     const used_product = await this.findById(UpdateUsed_ProductStateInput.id);
-    const user = await this.findUserById(context);
-    if (used_product.user.id !== user.id) {
+    const user = await this.userService.findById(user_id);
+    const now_user = await this.userService.findById(used_product.user.id);
+    if (now_user.id !== user.id) {
       throw new ForbiddenException(
         `본인이 작성한 게시글만 상태를 수정할 수 있습니다.`,
       );
     }
     used_product.state = UpdateUsed_ProductStateInput.state;
-    return this.usedProductRepository.save(used_product);
+    return await this.usedProductRepository.save(used_product);
   }
 
-  async delete(context: IContext, id: string): Promise<void> {
+  async delete(user_id: string, id: string): Promise<boolean> {
     const used_product = await this.findById(id);
+
     if (!used_product) {
       throw new NotFoundException(`Id가 ${id}인 것을 찾을 수 없습니다.`);
     }
 
     // 사용자 조회
-    const user = await this.findUserById(context);
-    if (used_product.user.id !== user.id) {
+    const user = await this.userService.findById(user_id);
+    const now_user = await this.userService.findById(used_product.user.id);
+    if (now_user.id !== user.id) {
       throw new ForbiddenException(
         `본인이 작성한 게시글만 삭제할 수 있습니다.`,
       );
     }
-    await this.usedProductRepository.delete(id);
+    const result = await this.usedProductRepository.delete(id);
+    return result.affected ? true : false;
   }
 
   async addViewToPost(
@@ -147,48 +160,43 @@ export class UsedProductService {
 
   async addLikeToPost(
     AddLikeProductInput: AddLikeProductInput,
-    context: IContext,
+    user_id: string,
   ): Promise<Used_product> {
     const used_product = await this.findById(AddLikeProductInput.id);
-    const like_user_recordRepository = getRepository(Like_user_record);
     const like_user_record = new Like_user_record();
-    const user = await this.findUserById(context);
-    const checkuser = await like_user_recordRepository.findOne({
-      where: { used_product: used_product, user: user },
+    const user = await this.userService.findById(user_id);
+    const checkuser = await this.likeUserRecordRepository.findOne({
+      where: { used_product: { id: used_product.id }, user: { id: user.id } },
+      relations: ['user', 'used_product'],
     });
 
     if (checkuser) {
       throw new ForbiddenException(`이미 찜한 게시글 입니다.`);
     }
-    used_product.view = used_product.view + 1;
+    used_product.like = used_product.like + 1;
     like_user_record.used_product = used_product;
     like_user_record.user = user;
 
-    await like_user_recordRepository.save(like_user_record);
-    return this.usedProductRepository.save(used_product);
+    await this.likeUserRecordRepository.save(like_user_record);
+    await this.usedProductRepository.save(used_product);
+    return this.findById(AddLikeProductInput.id);
   }
 
-  async removeLikeToPost(id: string, context: IContext): Promise<Used_product> {
+  async removeLikeToPost(id: string, user_id: string): Promise<Used_product> {
     const used_product = await this.findById(id);
-    const like_user_recordRepository = getRepository(Like_user_record);
-    const user = await this.findUserById(context);
-    const checkuser = await like_user_recordRepository.findOne({
-      where: { used_product: used_product, user: user },
+    const user = await this.userService.findById(user_id);
+    const checkuser = await this.likeUserRecordRepository.findOne({
+      where: { used_product: { id: used_product.id }, user: { id: user.id } },
+      relations: ['user', 'used_product'],
     });
     if (!checkuser) {
       throw new NotFoundException('찜을 하지 않았습니다.');
     }
 
-    used_product.view = used_product.view - 1;
+    used_product.like = used_product.like - 1;
 
-    await like_user_recordRepository.delete(checkuser.id);
-    return this.usedProductRepository.save(used_product);
-  }
-
-  async findUserById(context: IContext): Promise<User> {
-    const userRepository = getRepository(User);
-    return userRepository.findOne({
-      where: { id: context.req.user.id },
-    });
+    await this.likeUserRecordRepository.delete(checkuser.id);
+    await this.usedProductRepository.save(used_product);
+    return this.findById(id);
   }
 }
