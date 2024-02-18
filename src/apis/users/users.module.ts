02386c0a7a031ab9061b97da5f60eeb98564a296
ssarisong <thdtnwls2989@naver.com>
2024-02-18 17:56:27 +0900

    Update Area Relation Into User

--- src/apis/users/users.module.ts
+++ src/apis/users/users.module.ts
@@ -7,9 +7,22 @@ import { Token } from './entities/token.entity';
 import { JwtModule } from '@nestjs/jwt';
 import JwtAccessStrategy from './strategies/jwt-access.strategy';
 import { JwtRefreshStrategy } from './strategies/jwt-refresh.strategy';
+import { AreaService } from '../area/area.service';
+import { Sido } from '../area/entities/sido.entity';
+import { Sgng } from '../area/entities/sgng.entity';
+import { Dong } from '../area/entities/dong.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Token]), JwtModule.register({})],
-  providers: [UserService, UserResolver, JwtAccessStrategy, JwtRefreshStrategy],
+  imports: [
+    TypeOrmModule.forFeature([User, Token, Sido, Sgng, Dong]),
+    JwtModule.register({}),
+  ],
+  providers: [
+    UserService,
+    UserResolver,
+    AreaService,
+    JwtAccessStrategy,
+    JwtRefreshStrategy,
+  ],
 })
 export class UserModule {}
