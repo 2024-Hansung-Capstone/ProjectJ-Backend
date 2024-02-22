1bac8b430b0a9204ec03ab514e95f3efec199aa1
kimjunghwannn <aa2002dcks@naver.com>
2024-02-22 18:28:54 +0900

    board와 reply에서 사용하기 위하여 Like_user_record수정

--- src/apis/used_markets/entities/like_user_record.entity.ts
+++ src/apis/used_markets/entities/like_user_record.entity.ts
@@ -3,6 +3,7 @@ import { Entity, ManyToOne, PrimaryGeneratedColumn, JoinColumn } from 'typeorm';
 import { User } from '../../users/entities/user.entity';
 import { UsedProduct } from './used_product.entity';
 import { Board } from 'src/apis/boards/entities/board.entity';
+import { Reply } from 'src/apis/boards/entities/reply.entity';
 @Entity()
 @ObjectType()
 export class Like_user_record {
@@ -14,7 +15,21 @@ export class Like_user_record {
   @JoinColumn()
   user: User;
 
-  @ManyToOne(() => UsedProduct, (Used_products) => Used_products.Like_users)
+  @ManyToOne(() => UsedProduct, (Used_products) => Used_products.Like_users, {
+    nullable: true,
+  })
   @JoinColumn()
-  used_product: UsedProduct;
+  used_product: UsedProduct | null;
+
+  @ManyToOne(() => Board, (Board) => Board.likeUsers, {
+    nullable: true,
+  })
+  @JoinColumn()
+  board: Board | null;
+
+  @ManyToOne(() => Reply, (Reply) => Reply.likeUsers, {
+    nullable: true,
+  })
+  @JoinColumn()
+  reply: Reply | null;
 }
