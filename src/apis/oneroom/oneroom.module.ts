c95e0f23402a3a1dd1a24f50f592aadfe4c9db39
ssarisong <thdtnwls2989@naver.com>
2024-03-30 12:37:06 +0900

    Add OneroomScheduler into OneroomModule

--- src/apis/oneroom/oneroom.module.ts
+++ src/apis/oneroom/oneroom.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { OneRoomResolver } from './oneroom.resolver';
 import { CommonModule } from '../common.module';
+import { OneRoomScheduler } from './oneroom.scheduler';
 @Module({
   imports: [CommonModule],
-  providers: [OneRoomResolver],
+  providers: [OneRoomResolver, OneRoomScheduler],
 })
 export class OneRoomModule {}
