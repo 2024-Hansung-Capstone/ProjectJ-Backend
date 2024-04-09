890ce8167790f90ffc7d17ace0118b6e9f8ec7a8
ssarisong <thdtnwls2989@naver.com>
2024-04-09 22:28:19 +0900

    Add notification(100, 200, 201, 202)

--- src/apis/notifications/entities/notification.entity.ts
+++ src/apis/notifications/entities/notification.entity.ts
@@ -1,7 +1,7 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import { Board } from 'src/apis/boards/entities/board.entity';
-import { Reply } from 'src/apis/boards/entities/reply.entity';
 import { Letter } from 'src/apis/letters/entities/letter.entity';
+import { LikeUserRecord } from 'src/apis/like/entities/like_user_record.entity';
 import { UsedProduct } from 'src/apis/used_markets/entities/used_product.entity';
 import { User } from 'src/apis/users/entities/user.entity';
 import {
@@ -14,7 +14,7 @@ import {
 } from 'typeorm';
 
 @Entity()
-@ObjectType()
+@ObjectType({ description: '알림 정보' })
 export class Notification {
   @PrimaryGeneratedColumn('uuid')
   @Field(() => String, { description: '고유 ID' })
@@ -41,13 +41,13 @@ export class Notification {
   })
   board?: Board;
 
-  @ManyToOne(() => UsedProduct, { cascade: true, nullable: true })
+  @ManyToOne(() => LikeUserRecord, { cascade: true, nullable: true })
   @JoinColumn()
-  @Field(() => UsedProduct, {
+  @Field(() => LikeUserRecord, {
     nullable: true,
     description: '알림이 생성된 중고 상품 정보',
   })
-  product?: UsedProduct;
+  like?: LikeUserRecord;
 
   @Column()
   @Field(() => String, { description: '알림 코드(코드표 참조)' })
