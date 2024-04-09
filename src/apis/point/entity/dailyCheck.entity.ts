0a24a8ac94675331d18f7b4c202344d5422bd2a0
ssarisong <thdtnwls2989@naver.com>
2024-04-09 17:20:53 +0900

    Update point apis

---
+++ src/apis/point/entity/dailyCheck.entity.ts
@@ -0,0 +1,26 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import { User } from 'src/apis/users/entities/user.entity';
+import {
+  PrimaryGeneratedColumn,
+  Column,
+  Entity,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+@Entity()
+@ObjectType()
+export class DailyCheck {
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => String, { description: '고유 ID' })
+  id: string;
+
+  @ManyToOne(() => User, { cascade: true })
+  @JoinColumn()
+  @Field(() => User, { description: '출석한 사용자 정보' })
+  user: User;
+
+  @Column()
+  @Field(() => Date, { nullable: true, description: '출석한 날짜' })
+  checked_at: Date;
+}
--- src/apis/point/point.service.ts
+++ src/apis/point/point.service.ts
@@ -1,54 +1,75 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Point } from './entity/point.entity';
-import { Repository } from 'typeorm';
+import { DailyCheck } from './entity/dailyCheck.entity';
+import { LessThanOrEqual, MoreThanOrEqual, Repository } from 'typeorm';
+import { User } from '../users/entities/user.entity';
+import { UserService } from '../users/users.service';
+import { Role } from './entity/role.entity';
 
 @Injectable()
 export class PointService {
   constructor(
-    @InjectRepository(Point)
-    private readonly pointRepository: Repository<Point>,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    @InjectRepository(Role)
+    private readonly roleRepository: Repository<Role>,
+    @InjectRepository(DailyCheck)
+    private readonly dailyCheckRepository: Repository<DailyCheck>,
+    private readonly userService: UserService,
   ) {}
 
-  async findById(id: string): Promise<Point> {
-    return await this.pointRepository.findOne({
-      where: { id: id },
+  /**
+   * 포인트 증가 서비스 메서드
+   * @param user_id 사용자 ID
+   * @param point 증가할 포인트
+   * @returns 포인트가 증가된 사용자 정보
+   */
+  async increase(user_id: string, point: number): Promise<User> {
+    const user = await this.userService.findById(user_id);
+    if (!user) {
+      throw new Error('해당 사용자가 존재하지 않습니다');
+    }
+
+    const role = await this.findRoleByUserId(user_id);
+    if (role.code == 'admin') {
+      throw new Error('관리자는 포인트를 증가시킬 수 없습니다');
+    }
+
+    await this.userRepository.update(user_id, { point: user.point + point });
+    return user;
+  }
+
+  /**
+   * 사용자의 포인트에 따른 역할 정보 조회 서비스 메서드
+   * @param user_id 조회할 사용자 ID
+   * @returns 사용자의 포인트에 따른 역할 정보
+   */
+  async findRoleByUserId(user_id: string): Promise<Role> {
+    const user = await this.userService.findById(user_id);
+    if (!user) {
+      throw new Error('해당 사용자가 존재하지 않습니다');
+    }
+
+    const role = await this.roleRepository.findOne({
+      where: {
+        min_point: LessThanOrEqual(user.point),
+        max_point: MoreThanOrEqual(user.point),
+      },
     });
+
+    return role;
   }
 
-  async increasePoint(id: string, additionalPoint: number): Promise<Point> {
-    const point = await this.findById(id);
-    var n: number = 1;
-    if (!point) {
-      throw new Error('Point가 존재하지 않습니다');
-    }
-    point.value = point.value + additionalPoint;
-
-    await this.pointRepository.save(point);
-    await this.checkTitleByPoint(id);
-    return point;
-  }
-
-  async checkTitleByPoint(id: string): Promise<void> {
-    const point = await this.findById(id);
-    if (!point) {
-      throw new Error('Point가 존재하지 않습니다');
-    }
-    if (point.value >= 0 && point.value <= 1000) point.title = '자취어린이';
-    else if (point.value >= 1001 && point.value <= 2000)
-      point.title = '초보자취생';
-    else if (point.value >= 2001 && point.value <= 3000)
-      point.title = 'N년차자취생';
-    else if (point.value >= 3001 && point.value <= 4000)
-      point.title = '자취도사';
-    else point.title = '자취만렙';
-    await this.pointRepository.save(point);
-  }
-
-  async checkAttendance(id: string): Promise<boolean> {
-    const point = await this.findById(id);
-    if (!point) {
-      throw new Error('Point가 존재하지 않습니다');
+  /**
+   * 출석 체크 서비스 메서드
+   * 이미 출석한 경우 실패 처리
+   * @param user_id 사용자 ID
+   * @returns 출석 체크 성공 여부
+   */
+  async checkIn(user_id: string): Promise<boolean> {
+    const user = await this.userService.findById(user_id);
+    if (!user) {
+      throw new Error('해당 사용자가 존재하지 않습니다');
     }
 
     // 오늘 날짜 구하기
@@ -59,26 +80,33 @@ export class PointService {
     const formattedDate = `${year}-${month}-${date}`;
     const startOfDay = new Date(formattedDate);
 
-    if (!point.attendanceDates) {
-      point.attendanceDates = [];
-    }
-    const isAlreadyChecked = point.attendanceDates.some((attendanceDate) => {
-      // date를 Date 객체로 형변환하여 연도, 월, 일이 같은지 비교
-      const checkDate = new Date(attendanceDate);
-      return (
-        checkDate.getFullYear() === year &&
-        checkDate.getMonth() === today.getMonth() &&
-        checkDate.getDate() === date
-      );
+    const latestCheckedAt = await this.dailyCheckRepository.find({
+      where: { user: { id: user_id } },
+      order: { checked_at: 'DESC' },
+      take: 1,
     });
 
-    if (isAlreadyChecked) {
-      return false;
+    if (latestCheckedAt.length === 0) {
+      await this.dailyCheckRepository.save({
+        user: user,
+        checked_at: startOfDay,
+      });
+
+      await this.increase(user_id, 50);
+
+      return true;
     }
 
-    point.attendanceDates.push(startOfDay);
-    await this.pointRepository.save(point);
-    await this.increasePoint(id, 50);
+    if (latestCheckedAt[0].checked_at >= startOfDay) {
+      throw new Error('이미 출석했습니다');
+    }
+
+    await this.dailyCheckRepository.save({
+      user: user,
+      checked_at: startOfDay,
+    });
+
+    await this.increase(user_id, 50);
 
     return true;
   }
