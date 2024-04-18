316fa5a12c5453a5f55afdfa31d18a047b87e57a
kimjunghwannn <aa2002dcks@naver.com>
2024-04-18 23:50:19 +0900

    postImage entity 생성

---
+++ src/apis/post_image/entities/postImage.entity.ts
@@ -0,0 +1,40 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import { Board } from 'src/apis/boards/entities/board.entity';
+import { UsedProduct } from 'src/apis/used_markets/entities/used_product.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+@ObjectType()
+export class PostImage {
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => String, { description: '고유 ID' })
+  id: string;
+
+  @Field()
+  @Column({ type: 'varchar', length: 255 })
+  imagePath: string;
+
+  @JoinColumn()
+  @ManyToOne(() => Board, (Board) => Board.postImages, {
+    eager: false,
+    nullable: true,
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  })
+  board: Board;
+
+  @JoinColumn()
+  @ManyToOne(() => UsedProduct, (UsedProduct) => UsedProduct.postImages, {
+    eager: false,
+    nullable: true,
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  })
+  used_proudct: UsedProduct;
+}
