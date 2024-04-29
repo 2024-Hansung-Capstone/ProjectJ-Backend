edd40a252ee2ab569d5442dc4a73fc3ead5fa6ac
kimjunghwannn <aa2002dcks@naver.com>
2024-04-30 00:09:14 +0900

    savetoImgS3를 PostImage 서비스에 옮기기 위해 savetoImgS3를 BoardService에서 삭제

--- src/apis/boards/boards.service.ts
+++ src/apis/boards/boards.service.ts
@@ -20,13 +20,9 @@ import { LikeUserRecord } from '../like/entities/like_user_record.entity';
 import { Reply } from './entities/reply.entity';
 import { NotificationService } from '../notifications/notifications.service';
 import { PointService } from '../point/point.service';
-import * as AWS from 'aws-sdk';
-import * as path from 'path';
 import { PostImage } from '../post_image/entities/postImage.entity';
 @Injectable()
 export class BoardService {
-  private readonly awsS3: AWS.S3;
-  private readonly S3_BUCKET_NAME: string;
   constructor(
     @InjectRepository(Board)
     private readonly boardRepository: Repository<Board>,
@@ -42,14 +38,7 @@ export class BoardService {
     private readonly notificationService: NotificationService,
     @Inject(forwardRef(() => PointService))
     private readonly pointService: PointService,
-  ) {
-    this.awsS3 = new AWS.S3({
-      accessKeyId: process.env.AWS_S3_ACCESS_KEY,
-      secretAccessKey: process.env.AWS_S3_SECRET_KEY,
-      region: process.env.AWS_S3_REGION,
-    });
-    this.S3_BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME;
-  }
+  ) {}
 
   async findAll(category: string): Promise<Board[]> {
     return await this.boardRepository.find({
@@ -114,7 +103,7 @@ export class BoardService {
     const user = await this.userService.findById(user_id);
     const board = new Board();
 
-    const imgUrl: string | string[] = await this.saveImageToS3(folder, file);
+    const imgUrl: string | string[] = 'asd';
     if (Array.isArray(imgUrl)) {
       for (const url of imgUrl) {
         const postImage = new PostImage();
@@ -138,57 +127,6 @@ export class BoardService {
     board.user = user;
     await this.pointService.increase(user.id, +10);
     return await this.boardRepository.save(board);
-  }
-  async saveImageToS3(
-    folder: string,
-    file: Express.Multer.File | Express.Multer.File[],
-  ): Promise<string | string[]> {
-    try {
-      // 입력값이 파일 배열인 경우와 단일 파일인 경우를 구분하여 처리
-      if (Array.isArray(file)) {
-        const uploadedUrls: string[] = [];
-
-        for (const singleFile of file) {
-          const key = `${folder}/${Date.now()}_${path.basename(
-            singleFile.originalname,
-          )}`.replace(/ /g, '');
-
-          await this.awsS3
-            .putObject({
-              Bucket: this.S3_BUCKET_NAME,
-              Key: key,
-              Body: singleFile.buffer,
-              ACL: 'public-read',
-              ContentType: singleFile.mimetype,
-            })
-            .promise();
-
-          const imageUrl = `https://${process.env.AWS_S3_BUCKET_NAME}.s3.amazonaws.com/${key}`;
-          uploadedUrls.push(imageUrl);
-        }
-
-        return uploadedUrls;
-      } else {
-        const key = `${folder}/${Date.now()}_${path.basename(
-          file.originalname,
-        )}`.replace(/ /g, '');
-
-        await this.awsS3
-          .putObject({
-            Bucket: this.S3_BUCKET_NAME,
-            Key: key,
-            Body: file.buffer,
-            ACL: 'public-read',
-            ContentType: file.mimetype,
-          })
-          .promise();
-
-        return `https://${process.env.AWS_S3_BUCKET_NAME}.s3.amazonaws.com/${key}`;
-      }
-    } catch (error) {
-      console.error('이미지 업로드 중 에러사유:', error);
-      throw new Error('이미지 업로드에 에러 발생');
-    }
   }
 
   async update(
