68bad7fe89d0a757898b0bf52c7bf7a39753cd87
ssarisong <thdtnwls2989@naver.com>
2024-05-19 02:17:25 +0900

    Add area fetching apis

--- src/apis/area/area.resolver.ts
+++ src/apis/area/area.resolver.ts
@@ -1,6 +1,8 @@
 import { Args, Query, Resolver } from '@nestjs/graphql';
 import { AreaService } from './area.service';
 import { Dong } from './entities/dong.entity';
+import { Sgng } from './entities/sgng.entity';
+import { Sido } from './entities/sido.entity';
 
 @Resolver()
 export class AreaResolver {
@@ -13,5 +15,26 @@ export class AreaResolver {
     @Args('dong_name', { description: '행정동 이름' }) dong_name: string,
   ): Promise<Dong> {
     return await this.areaService.findDongByName(dong_name);
+  }
+
+  @Query(() => [Sido], {
+    description: '전체 시도 정보를 조회합니다.',
+  })
+  async fetchAllSido(): Promise<Sido[]> {
+    return await this.areaService.findAllSido();
+  }
+
+  @Query(() => [Sgng], {
+    description: '전체 시군구 정보를 조회합니다.',
+  })
+  async fetchAllSgng(): Promise<Sgng[]> {
+    return await this.areaService.findAllSgng();
+  }
+
+  @Query(() => [Dong], {
+    description: '전체 행정동 정보를 조회합니다.',
+  })
+  async fetchAllDong(): Promise<Dong[]> {
+    return await this.areaService.findAllDong();
   }
 }
